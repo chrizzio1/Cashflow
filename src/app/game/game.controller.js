86c6ebfdec282af918f635d90b75740a3f0e877f
Christian Nitsche <christian.nitsche@vapintar.com>
2016-03-25 21:18:33 +0100

    Some initial setup

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -8,7 +8,37 @@
   /** @ngInject */
   function GameController() {
     var vm = this;
+    vm.roll = roll;
+    vm.getPlayers = getPlayers;
 
+
+    // ######
+    var gameCompleted = false;
+    var players = {
+      'player1': { 'name': 'Chris' },
+      'player2': { 'name': 'Hendrik' }
+    };
+    var ratRace = [];
+
+    function gameLoop() {
+      while (!gameCompleted) {
+        // TODO
+        gameCompleted = !gameCompleted;
+      }
+    }
+
+    function roll() {
+      return Math.round(Math.random() * (6 - 1) + 1);
+    }
+
+    function getPlayers() {
+      return players;
+    }
+
+
+    function getRatrace() {
+      return ratRace;
+    }
 
   }
 })();
