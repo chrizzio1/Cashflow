07927d4678ca28586a5179befe839031f144dba7
Christian Nitsche <christian.nitsche@vapintar.com>
2016-03-28 21:29:14 +0200

    Katsching

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -10,20 +10,95 @@
     var vm = this;
     vm.roll = roll;
     vm.getPlayers = getPlayers;
+    vm.getCurrentRound = getCurrentRound;
 
 
     // ######
     var gameCompleted = false;
-    var players = {
-      'player1': { 'name': 'Chris' },
-      'player2': { 'name': 'Hendrik' }
-    };
-    var ratRace = [];
+    var players = [
+      { 'name': 'Chris' },
+      { 'name': 'Hendrik' }
+    ];
+    var currentPlayer = 0;
+
+    var ratRace = [{ 'type': 'deal' },
+      { 'type': 'charity' },
+      { 'type': 'deal' },
+      { 'type': 'payday' },
+      { 'type': 'deal' },
+      { 'type': 'event' },
+      { 'type': 'deal' },
+      { 'type': 'costs' },
+      { 'type': 'deal' },
+      { 'type': 'baby' },
+      { 'type': 'deal' },
+      { 'type': 'payday' },
+      { 'type': 'deal' },
+      { 'type': 'event' },
+      { 'type': 'deal' },
+      { 'type': 'cost' },
+      { 'type': 'deal' },
+      { 'type': 'debt' },
+      { 'type': 'deal' },
+      { 'type': 'payday' },
+      { 'type': 'deal' },
+      { 'type': 'event' },
+      { 'type': 'deal' },
+      { 'type': 'cost' }
+    ];
+
+    var currentRound = 0;
+
+    function getCurrentRound() {
+      return currentRound;
+    }
+
+    // Start game
+    gameLoop();
 
     function gameLoop() {
+
       while (!gameCompleted) {
         // TODO
-        gameCompleted = !gameCompleted;
+
+        var thisPlayer = players[currentPlayer];
+        // Runde
+
+        // 1. rolle the die
+        var rolledDice = roll();
+
+        // 2. Set player to new position
+        if (thisPlayer.position === undefined) {
+          thisPlayer.position = 0;
+        }
+        thisPlayer.position += rolledDice;
+        console.log(thisPlayer.name + " würfelt eine " + rolledDice);
+
+        // 3. Execute the Event on the new field
+        console.log(thisPlayer.name + " landet auf " + ratRace[thisPlayer.position].type);
+
+        // 4. Financial actions
+        // todo
+
+        // End of round
+        nextPlayer();
+
+
+        // console.log(players[currentPlayer].name)
+
+        currentRound++;
+
+        if (currentRound == 10) {
+          gameCompleted = true;
+        }
+      }
+    }
+
+    function nextPlayer() {
+      if (currentPlayer + 1 >= players.length) {
+        currentPlayer = 0;
+      } else {
+        currentPlayer++;
       }
     }
 
