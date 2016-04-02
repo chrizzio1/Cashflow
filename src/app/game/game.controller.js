a00255249f569e02c8ac0820dcc0f14ceb9ba108
Christian Nitsche <christian.nitsche@vapintar.com>
2016-04-02 11:19:26 +0200

    Fixing Roll() and roundLog

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
 
   angular
@@ -11,7 +11,6 @@
 
     var currentRound = 0;
     var ratRace = cfActions.getActions();
-
 
     vm.localPlayer = $localStorage.player;
 
@@ -28,7 +27,7 @@
     if (!$stateParams.game.$bindTo) {
       $state.go('menu');
     } else {
-      $stateParams.game.$bindTo(vm, 'game').then(function () {
+      $stateParams.game.$bindTo(vm, 'game').then(function() {
         //vm.game.players = [
         //  {name: 'Chris', cash: 200, position: 0},
         //  {name: 'Hendrik', cash: 300, position: 0}
@@ -42,23 +41,49 @@
         vm.startTurn = startTurn;
 
         function startTurn() {
-          roll()                        // 1. Roll the dice
-            .then(movePlayer)           // 2. Move player to new position
-            .then(executeFieldAction)   // 3. Execute the event on the new field
-            .then(checkGameState)       // 4. Check if a player wins
-            .then(financialStatement)   // 4. Financial actions
-            .then(nextPlayer);          // 5. Next player's turn
+          vm.game.roundLog === undefined ? vm.game.roundLog = [] : null;
+          roll()                      // 1. Roll the dice
+            .then(movePlayer)         // 2. Move player to new position
+            .then(executeFieldAction) // 3. Execute the event on the new field
+            .then(checkGameState)     // 4. Check if a player wins
+            .then(financialStatement) // 4. Financial actions
+            .then(nextPlayer);        // 5. Next player's turn
         }
 
-        function checkGameState() {
-          console.log('check state');
+        function roll() {
+          console.log('roll');
           var deferred = $q.defer();
+          var max = 6;
+          var min = 1;
+
+          vm.game.rolled = Math.floor(Math.random() * (max - min + 1) + min);
+          deferred.resolve();
+          return deferred.promise;
+        }
+
+        function movePlayer() {
+          console.log('move player');
+          var deferred = $q.defer();
+
+          (vm.game.currentPlayer.position === undefined) ? vm.game.currentPlayer.position = 0: '';
+          var log = vm.game.currentPlayer.name + ' würfelt eine ' + vm.game.rolled + ' und rückt von ' + vm.game.currentPlayer.position + ' auf ' + ratRace[(vm.game.currentPlayer.position + vm.game.rolled) % ratRace.length].type;
+          vm.game.roundLog.push({ content: log });
+          vm.game.currentPlayer.position = (vm.game.currentPlayer.position + vm.game.rolled) % ratRace.length;
+
+
           deferred.resolve();
           return deferred.promise;
         }
 
         function executeFieldAction() {
           console.log('field action');
+          var deferred = $q.defer();
+          deferred.resolve();
+          return deferred.promise;
+        }
+
+        function checkGameState() {
+          console.log('check state');
           var deferred = $q.defer();
           deferred.resolve();
           return deferred.promise;
@@ -71,31 +96,14 @@
           return deferred.promise;
         }
 
-        function movePlayer() {
-          console.log('move player');
-          var deferred = $q.defer();
-          var log = vm.game.currentPlayer.name + ' würfelt eine ' + vm.game.rolled + ' und rückt von ' + vm.game.currentPlayer.position + ' auf ' + (vm.game.currentPlayer.position + vm.game.rolled) % ratRace.length;
-          console.log(vm.game);
-          vm.game.roundLog.$add({content: log});
-          vm.game.currentPlayer.position = (vm.game.currentPlayer.position + vm.game.rolled) % ratRace.length;
-          deferred.resolve();
-          return deferred.promise;
-        }
-
         function nextPlayer() {
           console.log('next player');
+          console.log(vm.game.players)
           vm.game.currentPlayer = vm.game.players[++vm.game.currentPlayerIdx % vm.game.players.length];
           console.log('current player:');
           console.log(vm.game.currentPlayer);
         }
 
-        function roll() {
-          console.log('roll');
-          var deferred = $q.defer();
-          vm.game.rolled = Math.ceil(Math.random() * 6);
-          deferred.resolve();
-          return deferred.promise;
-        }
       });
     }
 
