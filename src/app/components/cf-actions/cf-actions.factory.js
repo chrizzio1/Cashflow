b55c748591be2bcb33a437e91d03bbd59d40482e
Christian Nitsche <christian.nitsche@vapintar.com>
2016-04-04 22:04:24 +0200

    KASTCHING!

--- src/app/components/cf-actions/cf-actions.factory.js
+++ src/app/components/cf-actions/cf-actions.factory.js
@@ -8,169 +8,169 @@
     var actions = [{
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'charity',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'payday',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'event',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'cost',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'baby',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'payday',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'event',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'cost',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'debt',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'payday',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'event',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'deal',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }, {
       'type': 'cost',
       event: function(player) {
-        var num = Math.floor(Math.random() * 2500) + 1;
-        num *= Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+        var num = Math.floor(Math.random() * 13000) + 1;
+        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
         return num;
       }
     }];
