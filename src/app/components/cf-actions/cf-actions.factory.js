0ed454a2489b03ad48691dbbf416c7044f35172b
Hendrik Bursian <hendrikbursian@gmail.com>
2016-04-05 00:15:25 +0200

    ActionCallbacks angelegt und Logausgaben angepasst Spielfelder abbilden

--- src/app/components/cf-actions/cf-actions.factory.js
+++ src/app/components/cf-actions/cf-actions.factory.js
@@ -1,185 +1,107 @@
-(function() {
+(function () {
   'use strict';
 
   angular
     .module('cashflow')
 
-  .factory('cfActions', [function() {
-    var actions = [{
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'charity',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'payday',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'event',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'cost',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'baby',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'payday',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'event',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'cost',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'debt',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'payday',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'event',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'deal',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }, {
-      'type': 'cost',
-      event: function(player) {
-        var num = Math.floor(Math.random() * 13000) + 1;
-        num *= Math.floor(Math.random() * 8) == 1 ? -1 : 1;
-        return num;
-      }
-    }];
+    .factory('cfActions', [function () {
+      var actions = [
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'charity', event: charityCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'payday', event: paydayCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'event', event: eventCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'cost', event: costCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'baby', event: babyCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'payday', event: paydayCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'event', event: eventCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'cost', event: costCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'debt', event: debtCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'payday', event: paydayCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'event', event: eventCallback },
+        { 'type': 'deal', event: dealCallback },
+        { 'type': 'cost', event: costCallback }
+      ];
 
-    return {
-      getActions: function getActions() {
-        return actions;
-      }
-    };
-  }])
+      return {
+        getActions: function getActions() {
+          return actions;
+        }
+      };
 
+      function babyCallback(game) {
+        game.roundLog.push({ content: 'Baby!' });
+        // 50% +/- 20%
+        subtractRelativeCash(game, 0.50 + (Math.random() * 0.40 - 0.20));
+      }
+
+      function charityCallback(game) {
+        game.roundLog.push({ content: 'Charity!' });
+        subtractRelativeCash(game, 0.05);
+      }
+
+      function costCallback(game) {
+        game.roundLog.push({ content: 'Cost!' });
+        var amounts = [200, 577, 1233, 50, 66, 1, 999];
+        subtractAbsoluteCash(game, amounts[Math.floor(Math.random() * amounts.length)]);
+      }
+
+      function dealCallback(game) {
+        game.roundLog.push({ content: 'Deal!' });
+        addRelativeCash(game, Math.random() * 0.45);
+      }
+
+      function debtCallback(game) {
+        game.roundLog.push({ content: 'Debt!' });
+        // 7% +/- 5%
+        subtractRelativeCash(game, 0.07 + (Math.random() * 0.10 - 0.05));
+      }
+
+      function eventCallback(game) {
+        game.roundLog.push({ content: 'Event!' });
+        if (Math.random() < 0.5)
+          addRelativeCash(game, Math.random() * 0.40);
+        else
+          subtractRelativeCash(game, Math.random() * 0.30);
+      }
+
+      function paydayCallback(game) {
+        game.roundLog.push({ content: 'Payday!' });
+        addAbsoluteCash(game, 2000);
+      }
+
+      // TODO: Evtl. in Playerprototype
+      function addAbsoluteCash(game, amount) {
+        var currentPlayer = game.players[game.currentPlayerIdx];
+        game.roundLog.push({ content: currentPlayer.name + ' bekommt ' + amount + '!' });
+        currentPlayer.cash += amount;
+      }
+
+      function subtractAbsoluteCash(game, amount) {
+        var currentPlayer = game.players[game.currentPlayerIdx];
+        game.roundLog.push({ content: currentPlayer.name + ' verliert ' + amount + '!' });
+        currentPlayer.cash -= amount;
+      }
+
+      function addRelativeCash(game, percent) {
+        var currentPlayer = game.players[game.currentPlayerIdx];
+        game.roundLog.push({ content: currentPlayer.name + ' bekommt ' + Math.round(percent * 100) + '% (' + Math.round(currentPlayer.cash * percent) + ')!' });
+        currentPlayer.cash += Math.round(currentPlayer.cash * percent);
+      }
+
+      function subtractRelativeCash(game, percent) {
+        var currentPlayer = game.players[game.currentPlayerIdx];
+        game.roundLog.push({ content: currentPlayer.name + ' verliert ' + Math.round(percent * 100) + '% (-' + Math.round(currentPlayer.cash * percent) + ')!' });
+        currentPlayer.cash -= Math.round(currentPlayer.cash * percent);
+      }
+    }])
 })();
--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -99,19 +99,8 @@
       console.debug('field action');
 
       var deferred = $q.defer();
-      var currentPlayer = vm.game.players[vm.game.currentPlayerIdx];
-      var cash = ratRace[currentPlayer.position].event();
+      ratRace[vm.game.players[vm.game.currentPlayerIdx].position].event(vm.game);
 
-      currentPlayer.cash += cash;
-
-
-      if (cash > 0) {
-        var log = currentPlayer.name + ' casht ab: +' + cash + ' €!';
-        audioKatsching.play();
-      } else {
-        var log = currentPlayer.name + ' verliert ' + cash + ' €. LAPPEN!';
-      }
-      vm.game.roundLog.push({ content: log });
       vm.game.$save().then(deferred.resolve);
       return deferred.promise;
     }
